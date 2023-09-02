f44d4eeeb31276361b3c9bd759896e0076ffb7ab
Citosh <chaplinskyartem@gmail.com>
2023-09-02 13:44:16 +0300

    added changeemail + changepass endpoint

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const sequelize = require("sequelize")
 const express = require("express")
 const auth_router = require("./auth/auth_router")
 const admin_router = require("./admin/admin_router")
+const user_router = require("./user/user_router")
 const data_gen_router = require("./data_generation/data_gen_router")
 let cors = require("cors");
 const {testDbConnection} = require("./db/db_connection")
@@ -17,7 +18,7 @@ app.use(cors())
 app.use('/auth',auth_router)
 app.use('/admin', admin_router)
 app.use('/data', data_gen_router)
-
+app.use('/user', user_router)
 
 
 
